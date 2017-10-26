c73933d4ac400438572b1912ba9506875789c852
tomrimk <rimk.tom@gmail.com>
2017-10-26 15:55:39 +0300

    pridetas process.env.PORT

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("The YelpCamp Server has started");
 });
